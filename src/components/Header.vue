861bacee72ab5f6ad2820ba6a6f1e86c98ba98f7
aaxxa <aaxxa@dev.aaxxa>
2020-03-16 16:57:57 +0200

    second commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+
+	<div>
+	  <b-nav tabs>
+	    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+	    <b-nav-item disabled>Counter: {{numCorrect}} / {{numTotal}}</b-nav-item>
+	  </b-nav>
+	</div>
+
+</template>
+
+
+<script type="text/javascript">
+	export default {
+		props: [
+			'numCorrect',
+			'numTotal'
+		]
+	}
+</script>
